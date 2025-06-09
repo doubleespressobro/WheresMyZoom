e299d26a047725d2591f98e7063dd02caa950a49
Arecurius <36637378+Arecurius0@users.noreply.github.com>
2025-06-09 16:10:57 +0200

    Added the auto apply on launch logic from the poe2 version

--- WheresMyZoomSettings.cs
+++ WheresMyZoomSettings.cs
@@ -1,4 +1,5 @@
-﻿using ExileCore.Shared.Interfaces;
+﻿using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
 using ExileCore.Shared.Nodes;
 
 namespace WheresMyZoom;
@@ -7,10 +8,36 @@ public class WheresMyZoomSettings : ISettings
 {
     public ToggleNode Enable { get; set; } = new ToggleNode(false);
 
+    [Menu("Zoom Menu")]
+    public ZoomMenu ZoomMenu { get; set; } = new ZoomMenu();
+
+    [Menu("QOL Menu")]
+    public QOLMenu QOLMenu { get; set; } = new QOLMenu();
+}
+
+[Submenu(CollapsedByDefault = false)]
+public class ZoomMenu
+{
+    [Menu("Enable Unzoom", "Scroll Mouse Wheel to unzoom further.")]
     public ButtonNode EnableZoom { get; set; } = new ButtonNode();
+    [Menu("Enable Unzoom at Launch", "Scroll Mouse Wheel to unzoom further.")]
+    public ToggleNode EnableZoomAtLaunch { get; set; } = new ToggleNode(false);
+
+    [Menu("Enable Fast Zoom", "Scroll Mouse Wheel unzoom faster.")]
     public ButtonNode EnableFastZoom { get; set; } = new ButtonNode();
+    [Menu("Enable Fast Zoom at Launch", "Scroll Mouse Wheel unzoom faster.")]
+    public ToggleNode EnableFastZoomAtLaunch { get; set; } = new ToggleNode(false);
+}
+
+[Submenu(CollapsedByDefault = false)]
+public class QOLMenu
+{
     public ButtonNode EnableNoFog { get; set; } = new ButtonNode();
+    public ToggleNode EnableNoFogAtLaunch { get; set; } = new ToggleNode(false);
+
     public ButtonNode EnableNoBlackBox { get; set; } = new ButtonNode();
+    public ToggleNode EnableNoBlackBoxAtLaunch { get; set; } = new ToggleNode(false);
+
     public ButtonNode EnableBrightness { get; set; } = new ButtonNode();
-    
+    public ToggleNode EnableBrightnessAtLaunch { get; set; } = new ToggleNode(false);
 }
